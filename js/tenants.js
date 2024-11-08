5ba7f67df4776ac000b58c81c3f9e201f82dfbd0
lfernandogonzalez <gofernando83@gmail.com>
2024-11-08 16:15:20 -0500

    cors

--- js/tenants.js
+++ js/tenants.js
@@ -1,18 +1,29 @@
+// Function to open the modal
+function open_add_tenant() {
+    document.getElementById("add_tenant_modal").style.display = "block";
+    document.getElementById("add_tenant_modal").style.display = "block";
+}
 
+
+  // Function to close the modal
+  function close_add_tenant() {
+    document.getElementById("add_tenant_modal").style.display = "none";
+    document.getElementById("modal").style.display = "none";
+  }
+  
 function add_tenant() {
     const formData = Object.fromEntries([
         'address','unit','email', 'first_name', 'last_name', 'phone_number','lease_start','lease_end','notes'
     ].map(field => {
-            return [field, document.getElementById(`add_tenant_${field}`).value];
-        }
+        return [field, document.getElementById(`add_tenant_${field}`).value];
     }));
     
-    fetch('https://baf4kiept7.execute-api.us-east-1.amazonaws.com/prod', {
+    fetch('https://yg5db2uq93.execute-api.us-east-1.amazonaws.com/prod/employee', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData)
     })
     .then(response => response.ok ? response.json() : Promise.reject('Failed to add member'))
-    .then(response => (console.log(response), open_add_member(), get_members()))
+    .then(response => (console.log(response)))
     .catch(error => console.error('Error:', error));
 }
